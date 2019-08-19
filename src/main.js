7478b9edb323baecd3e91069cc4e02c7fc5cd772
mirzaev.r.a <mirzaev.r.a@gmail.com>
2019-08-19 23:56:02 +0300

    валидация логина

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import dateFilter from './filters/date.filter'
-import './registerServiceWorker'
-import 'materialize-css/dist/js/materialize.min'
+import Vue from 'vue';
+import Vuelidate from "vuelidate";
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import dateFilter from './filters/date.filter';
+import './registerServiceWorker';
+import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
-Vue.filter('date', dateFilter)
+
+Vue.use(Vuelidate);
+Vue.filter('date', dateFilter);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,21 +4,41 @@
             <span class="card-title">Домашняя бухгалтерия</span>
             <div class="input-field">
                 <input
+                        v-model.trim="email"
                         id="email"
                         type="text"
-                        class="validate"
+                        :class="{invalid: emailCheck}"
                 >
                 <label for="email">Email</label>
-                <small class="helper-text invalid">Email</small>
+                <small
+                        v-if="$v.email.$dirty && !$v.email.required"
+                        class="helper-text invalid"
+                >Поле Email не должно быть пустым
+                </small>
+                <small
+                        v-if="$v.email.$dirty && !$v.email.email"
+                        class="helper-text invalid"
+                >Введите корректный Email
+                </small>
             </div>
             <div class="input-field">
                 <input
+                        v-model.trim="password"
                         id="password"
                         type="password"
-                        class="validate"
+                        :class="{invalid: passwordCheck}"
                 >
                 <label for="password">Пароль</label>
-                <small class="helper-text invalid">Password</small>
+                <small
+                        v-if="$v.password.$dirty && !$v.password.required"
+                        class="helper-text invalid"
+                >Введите пароль
+                </small>
+                <small
+                        v-if="$v.password.$dirty && !$v.password.minLength"
+                        class="helper-text invalid"
+                >Пароль должен состоять из {{ $v.password.$params.minLength.min }} и более символов. Сейчас он {{ password.length }}
+                </small>
             </div>
         </div>
         <div class="card-action">
@@ -41,14 +61,58 @@
 </template>
 
 <script>
-    export default {
-        name: "Login",
-        methods: {
-            submitHandler() {
-                this.$router.push('/')
-            }
+  import {email, required, minLength} from 'vuelidate/lib/validators';
+
+  export default {
+    name: "Login",
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    validations: {
+      email: {
+        email,
+        required
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    },
+    computed: {
+      emailCheck() {
+        if ((this.$v.email.$dirty && !this.$v.email.required) || (this.$v.email.$dirty && !this.$v.email.email)) {
+          return true;
         }
+        return false;
+      },
+      passwordCheck() {
+        if ((this.$v.password.$dirty && !this.$v.password.required) || (this.$v.password.$dirty && !this.$v.password.minLength)) {
+          return true;
+        }
+        return false;
+      }
+    },
+    methods: {
+      submitHandler() {
+        if (this.$v.$invalid) {
+          this.$v.$touch();
+          return;
+        }
+
+        const formData = {
+          email: this.email,
+          password: this.password
+        }
+
+        console.log(formData);
+
+        this.$router.push('/');
+      }
     }
+  };
 </script>
 
 <style scoped>
